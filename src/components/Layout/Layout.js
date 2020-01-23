00abdfd4c631556105fd6bb06fa06e9c102d401b
Lucas Eduardo <lucased78@hotmail.com>
2020-01-23 09:26:31 -0300

    [FEATURE] Building the BurgerIngredient component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Aux from '../../hoc/Aux'
+import classes from './Layout.module.css'
+
+const Layout = props => (
+  <Aux>
+      <div>
+        Toolbar, Sidedrawer, Backdrop
+      </div>
+
+      <div className={classes.Content}>{ props.children }</div>
+  </Aux>
+);
+
+export default Layout;
